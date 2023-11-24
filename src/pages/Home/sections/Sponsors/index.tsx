73ad6c44784ee655dafbf24b3185a7a09a0c2914
Jay Whitsitt <jay.whitsitt@gmail.com>
2023-11-24 14:56:33 -0600

    Correct order of words in name

--- src/pages/Home/sections/Sponsors/index.tsx
+++ src/pages/Home/sections/Sponsors/index.tsx
@@ -21,7 +21,7 @@ const Sponsors: FC = () => {
         </div>
 
         <div className="action container-thin">
-          <span>{`Meet the organizations that make ${DevfestDetails.location} ${DevfestDetails.name} possible. If you’d like to learn more about sponsorships, read our `}</span>
+          <span>{`Meet the organizations that make ${DevfestDetails.name} ${DevfestDetails.location} possible. If you’d like to learn more about sponsorships, read our `}</span>
           <a href={config?.event?.sponsors?.prospectus}>Sponsor Prospectus</a>
           <span> or </span>
           <a href={config?.org ? `mailto:${config.org.email}` : ''}>email us</a>
