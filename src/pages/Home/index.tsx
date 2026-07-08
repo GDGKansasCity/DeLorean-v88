3490a697954ecae5c2d8273cd61a2e57670f7e66
Jay Whitsitt <jay.whitsitt@gmail.com>
2026-07-07 21:49:30 -0500

    Adjust heading sizes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,11 +53,11 @@ const Home: FC<HomeProps> = ({ config, showTickets, featureProspectus }) => {
         <div className="container">
           <Logo className="event-logo mb-4"/>
 
-          <h1 className="container-thin">
+          <h3 className="container-thin">
             {DevfestDetails.description}
-          </h1>
+          </h3>
 
-          <h3>{startDate && format(startDate, 'EEEE, MMMM d, yyyy')}</h3>
+          <h1>{startDate && format(startDate, 'EEEE, MMMM d, yyyy')}</h1>
           <h3>{config?.venue?.name}</h3>
 
           {showTickets && (
